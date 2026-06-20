3b7d2f13da0429d5aafc920a8438bdbb06ac53db
Awezkhan0 <awez244k@gmail.com>
2026-06-20 17:09:30 +0100

    Implement visitor counter function in Azure Function

--- api/function_app.py
+++ api/function_app.py
@@ -1,1 +1,34 @@
+import azure.functions as func
+import logging
+from azure.data.tables import TableServiceClient
+import os
+import json
 
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="visitor-counter")
+def visitor_counter(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Visitor counter function triggered.')
+
+    connection_string = os.environ["COSMOS_CONNECTION_STRING"]
+    table_service = TableServiceClient.from_connection_string(conn_str=connection_string)
+    table_client = table_service.get_table_client(table_name="visitors")
+
+    try:
+        entity = table_client.get_entity(partition_key="counter", row_key="visits")
+        count = entity["Count"] + 1
+        entity["Count"] = count
+        table_client.update_entity(entity)
+    except:
+        count = 1
+        table_client.create_entity({
+            "PartitionKey": "counter",
+            "RowKey": "visits",
+            "Count": count
+        })
+
+    return func.HttpResponse(
+        json.dumps({"count": count}),
+        mimetype="application/json",
+        headers={"Access-Control-Allow-Origin": "*"}
+    )
